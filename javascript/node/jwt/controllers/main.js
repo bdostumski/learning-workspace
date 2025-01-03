79abd19443de95118020f737d2d836ade77e5b53
Borislav Dostumski <b.dostumski@gmail.com>
2025-01-03 12:01:43 +0200

    verify token

--- javascript/node/jwt/controllers/main.js
+++ javascript/node/jwt/controllers/main.js
@@ -18,11 +18,23 @@ const login = async (req, res) => {
 }
 
 const dashboard = async (req, res) => {
-  const luckyNumber = Math.floor(Math.random() * 100);
-  res.status(200).json({
-    msg: `Hello John Doe`,
-    luckyNumber: `Your lucky number is ${luckyNumber}`
-  })
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer '))
+    throw new CustomApiError('Please provide token', 401);
+
+  try {
+    const token = authHeader.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const luckyNumber = Math.floor(Math.random() * 100);
+    res.status(200).json({
+      msg: `Hello ${decoded.username}`,
+      secret: `Here is your authorized data, your lucky number is ${luckyNumber}`
+    });
+  } catch (error) {
+    throw new CustomApiError('Not authorized', 401);
+  }
 }
 
 module.exports = {
