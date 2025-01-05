c6f6fb3fb57ca24f5fd6073772be42fb5aa02052
Borislav Dostumski <b.dostumski@gmail.com>
2025-01-05 10:01:30 +0200

    get all jobs for the user

--- programming/javascript/node/jobs-api/controllers/jobs.js
+++ programming/javascript/node/jobs-api/controllers/jobs.js
@@ -3,7 +3,8 @@ const { StatusCodes } = require('http-status-codes');
 const { BadRequestError, NotFoundError } = require('../errors');
 
 const getAllJobs = async (req, res) => {
-  res.status(StatusCodes.OK).json(req.user);
+  const jobs = await Job.find({ createdBy: req.user.userId  }).sort('createdAt');
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.lenght });
 }
 
 const getJob = async (req, res) => {
