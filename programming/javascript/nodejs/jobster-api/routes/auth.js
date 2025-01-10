b28d7628cd276a2c5862c63fedcba17a89dc11a0
Borislav Dostumski <b.dostumski@gmail.com>
2025-01-10 22:04:48 +0200

    Update User

--- programming/javascript/nodejs/jobster-api/routes/auth.js
+++ programming/javascript/nodejs/jobster-api/routes/auth.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { register, login } = require('../controllers/auth')
+const authUser = require('../middleware/authentication')
+const { register, login, updateUser } = require('../controllers/auth')
+
 router.post('/register', register)
 router.post('/login', login)
+router.patch('/updateUser', authUser, updateUser)
 
 module.exports = router
