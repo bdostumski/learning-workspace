18a0bcb9ed1c6a426f17edb995171dca09802d44
Borislav Dostumski <b.dostumski@gmail.com>
2025-01-11 10:22:37 +0200

    api limiter

--- programming/javascript/nodejs/jobster-api/routes/auth.js
+++ programming/javascript/nodejs/jobster-api/routes/auth.js
@@ -4,8 +4,17 @@ const authUser = require('../middleware/authentication')
 const demoUser = require('../middleware/demo-user')
 const { register, login, updateUser } = require('../controllers/auth')
 
-router.post('/register', register)
-router.post('/login', login)
+const rateLimiter = require('express-rate-limit')
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10,
+  message: {
+    msg: 'Too many requests from this IP, please try again after 15 minutes'
+  }
+})
+
+router.post('/register', apiLimiter, register)
+router.post('/login', apiLimiter, login)
 router.patch('/updateUser', authUser, demoUser, updateUser)
 
 module.exports = router
