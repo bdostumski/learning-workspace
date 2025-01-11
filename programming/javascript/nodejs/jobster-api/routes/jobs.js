9849c6fe154b6d13a610a1b1f4935d5a4c6313cb
Borislav Dostumski <b.dostumski@gmail.com>
2025-01-11 13:55:21 +0200

    jobs statistics

--- programming/javascript/nodejs/jobster-api/routes/jobs.js
+++ programming/javascript/nodejs/jobster-api/routes/jobs.js
@@ -8,9 +8,11 @@ const {
   getAllJobs,
   updateJob,
   getJob,
+  showStats
 } = require('../controllers/jobs')
 
 router.route('/').post(demoUser, createJob).get(getAllJobs)
+router.route('/stats').get(showStats)
 router.route('/:id').get(getJob).delete(demoUser, deleteJob).patch(demoUser, updateJob)
 
 module.exports = router
