94f00f175cd8edce838b2ee65d40f9b096e93155
Borislav Dostumski <b.dostumski@gmail.com>
2025-01-12 12:33:16 +0200

    check for errors in upload controller

--- programming/javascript/nodejs/file-image-upload/controllers/productController.js
+++ programming/javascript/nodejs/file-image-upload/controllers/productController.js
@@ -3,27 +3,16 @@ const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors'):
 
 const createProduct = async (req, res) => {
-  if (!req.files)
-    throw new CustomError(StatusCodes.BAD_REQUEST, 'Please upload a file');
-
   const product = await Product.create(req.body);
-
-  if (!product.image.mime.startsWith('image'))
-    throw new CustomError(StatusCodes.BAD_REQUEST, 'Please upload an image file');
-
-  const maxSize = 1024 * 1024;
-  if(product.image.size > maxSize)
-    throw new CustomError(StatusCodes.BAD_REQUEST, `Please upload an image less than ${maxSize}kb`);
-
   return res.status(StatusCodes.CREATED).json({ product });
+}
 
-  const getAllProducts = async (req, res) => {
-    const products = await Product.find({});
-    res.status(StatusCodes.OK).json({ products });
-  }
+const getAllProducts = async (req, res) => {
+  const products = await Product.find({});
+  res.status(StatusCodes.OK).json({ products });
+}
 
-  module.exports = {
-    createProduct,
-    getAllProducts
-  }
+module.exports = {
+  createProduct,
+  getAllProducts
 }
--- programming/javascript/nodejs/file-image-upload/controllers/uploadsController.js
+++ programming/javascript/nodejs/file-image-upload/controllers/uploadsController.js
@@ -4,10 +4,21 @@ const Product = require('../models/Product');
 const { StatusCodes } = require('http-status-codes');
 
 const uploadProductImage = async (req, res) => {
+  if (!req.files)
+    throw new CustomError(StatusCodes.BAD_REQUEST, 'Please upload a file');
+
   const productImage = req.files.image;
-  const imagePath = path.join(__dirname, '../public/uploads/' + `${productImage.name}`);
-  await productImage.mv(imagePath);
-  res.status(StatusCodes.OK).json({ image: { src: `/uploads/${productImage.name}`}});
+
+  if (!productImage.image.mime.startsWith('image'))
+    throw new CustomError(StatusCodes.BAD_REQUEST, 'Please upload an image file');
+
+  const maxSize = 1024 * 1024;
+  if(productImage.image.size > maxSize)
+    throw new CustomError(StatusCodes.BAD_REQUEST, `Please upload an image less than ${maxSize}kb`);
+
+  const imagePath = path.join(__dirname, '../public/uploads/' + `${productImageImage.name}`);
+  await productImageImage.mv(imagePath);
+  res.status(StatusCodes.OK).json({ image: { src: `/uploads/${productImageImage.name}`}});
 }
 
 module.exports = { uploadProductImage }
