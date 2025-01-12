2c360d3f8864a7e72459123980040753fb444eb0
Borislav Dostumski <b.dostumski@gmail.com>
2025-01-12 12:25:46 +0200

    data validation and throw custom errors

--- programming/javascript/nodejs/file-image-upload/controllers/productController.js
+++ programming/javascript/nodejs/file-image-upload/controllers/productController.js
@@ -1,17 +1,29 @@
 const Product = require('../models/Product');
 const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors'):
 
 const createProduct = async (req, res) => {
+  if (!req.files)
+    throw new CustomError(StatusCodes.BAD_REQUEST, 'Please upload a file');
+
   const product = await Product.create(req.body);
-  return res.status(StatusCodes.CREATED).json( { product } );
-}
 
-const getAllProducts = async (req, res) => {
-  const products = await Product.find({});
-  res.status(StatusCodes.OK).json( { products } );
-}
+  if (!product.image.mime.startsWith('image'))
+    throw new CustomError(StatusCodes.BAD_REQUEST, 'Please upload an image file');
 
-module.exports = {
-  createProduct,
-  getAllProducts
+  const maxSize = 1024 * 1024;
+  if(product.image.size > maxSize)
+    throw new CustomError(StatusCodes.BAD_REQUEST, `Please upload an image less than ${maxSize}kb`);
+
+  return res.status(StatusCodes.CREATED).json({ product });
+
+  const getAllProducts = async (req, res) => {
+    const products = await Product.find({});
+    res.status(StatusCodes.OK).json({ products });
+  }
+
+  module.exports = {
+    createProduct,
+    getAllProducts
+  }
 }
