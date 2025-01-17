671239e1b750a3ed84be1e8f2b86427e3810035e
Borislav Dostumski <b.dostumski@gmail.com>
2025-01-17 20:44:52 +0200

    add morgan route messages and error handling

--- programming/javascript/nodejs/e-commerce-api/app.js
+++ programming/javascript/nodejs/e-commerce-api/app.js
@@ -1,14 +1,35 @@
 // app config
 require('dotenv').config();
+require('express-async-errors');
 
 // express
 const express = require('express');
 const app = express();
 const port = process.env.APP_PORT || 3000;
 
+// other packages
+const morgan = require('morgan');
+
 // database
 const connectDB = require('./database/connect');
 
+// middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(morgan('tiny'));
+app.use(express.json());
+
+// route
+app.get('/', (req, res) => {
+  res.send('e-commerce api');
+});
+
+// error handling
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+// start server
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
