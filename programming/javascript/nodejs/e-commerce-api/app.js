4978acd1f65e827cadfbebd0732733fe90b9a7cb
Borislav Dostumski <b.dostumski@gmail.com>
2025-01-17 20:03:55 +0200

    connect to database

--- programming/javascript/nodejs/e-commerce-api/app.js
+++ programming/javascript/nodejs/e-commerce-api/app.js
@@ -1,17 +1,23 @@
+// app config
+require('dotenv').config();
+
+// express
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.APP_PORT || 3000;
+
+// database
+const connectDB = require('./database/connect');
 
 const start = async () => {
-
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
   } catch (error) {
     console.log(error);
   }
-
 };
 
 start();
