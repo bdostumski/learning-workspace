4c9a893eb31b8f2f3789543dcb9d4bb59c5b936c
Borislav Dostumski <b.dostumski@gmail.com>
2025-01-19 13:26:14 +0200

    check for unique email

--- programming/javascript/nodejs/e-commerce-api/controllers/authController.js
+++ programming/javascript/nodejs/e-commerce-api/controllers/authController.js
@@ -4,6 +4,7 @@ const CustomError = require('../errors');
 
 const register = async (req, res) => {
   const { email } = req.body;
+
   const emailAlreadyExists = await User.findOne({ email });
   if(emailAlreadyExists)
     throw new CustomError.BadRequestError('Email already exists');
