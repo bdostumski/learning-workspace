98f23a0b43fcc575973b42faa8d901ef0c042d2b
Borislav Dostumski <b.dostumski@gmail.com>
2025-01-31 19:40:36 +0200

    create product endpoint

--- programming/javascript/nodejs/e-commerce-api/controllers/productController.js
+++ programming/javascript/nodejs/e-commerce-api/controllers/productController.js
@@ -1,5 +1,11 @@
+const Product = require('../models/Product');
+const CustomError = require('../errors');
+const { StatusCodes } = require('http-status-codes');
+
 const createProduct = async (req, res) => {
-  res.send('createProduct');
+  req.body.user = req.user.userId;
+  const product = await Product.create(req.body);
+  res.status(StatusCodes.CREATED).json({ product });
 }
 
 const getAllProducts = async (req, res) => {
