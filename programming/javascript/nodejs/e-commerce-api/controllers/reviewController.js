4fda479d21ffc5289c90140f28cf4629826ea234
Borislav Dostumski <b.dostumski@gmail.com>
2025-02-01 06:13:43 +0200

    create review controllers

---
+++ programming/javascript/nodejs/e-commerce-api/controllers/reviewController.js
@@ -0,0 +1,31 @@
+const Review = require('../models/review');
+const CustomError = require('../errors');
+const StatusCodes = require('http-status-codes');
+
+const createReview = async (req, res) => {
+  req.send('createReview');
+}
+
+const getAllReviews = async (req, res) => {
+  req.send('getAllReviews');
+}
+
+const getSingleReview = async (req, res) => {
+  req.send('getSingleReview');
+}
+
+const updateReview = async (req, res) => {
+  req.send('updateReview');
+}
+
+const deleteReview = async (req, res) => {
+  req.send('deleteReview');
+}
+
+module.exports = {
+  createReview,
+  getAllReviews,
+  getSingleReview,
+  updateReview,
+  deleteReview
+}
