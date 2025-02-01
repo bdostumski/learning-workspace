06480ca0acb78606ff8e7760ee7a37a48443c6b8
Borislav Dostumski <b.dostumski@gmail.com>
2025-02-01 10:15:59 +0200

    delete review

--- programming/javascript/nodejs/e-commerce-api/controllers/reviewController.js
+++ programming/javascript/nodejs/e-commerce-api/controllers/reviewController.js
@@ -2,7 +2,7 @@ const Review = require('../models/Review');
 const Product = require('../models/Product');
 const StatusCodes = require('http-status-codes');
 const CustomError = require('../errors');
-const { checkPermissions } = require('../utils');
+const checkPermissions = require('../utils/checkPermissions');
 
 const createReview = async (req, res) => {
   const { product: productId } = req.body;
@@ -37,7 +37,13 @@ const updateReview = async (req, res) => {
 }
 
 const deleteReview = async (req, res) => {
-  res.send('deleteReview');
+  const { id: reviewId } = req.params;
+  const review = await Review.findOne({ _id: reviewId });
+  if(!review)
+    throw new CustomError.NotFoundError(`No review with id : ${reviewId}`);
+  checkPermissions(req.user, review.user);
+  await review.remove();
+  res.status(StatusCodes.OK).json({ msg: 'Review deleted successfully'});
 }
 
 module.exports = {
