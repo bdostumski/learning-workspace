607b84e9931e32efff0c0138fd1870840730e118
Borislav Dostumski <b.dostumski@gmail.com>
2025-02-01 06:20:28 +0200

    add review routes to app.js

--- programming/javascript/nodejs/e-commerce-api/app.js
+++ programming/javascript/nodejs/e-commerce-api/app.js
@@ -19,6 +19,7 @@ const connectDB = require('./database/connect');
 const authRouter = require('./routes/authRoute');
 const userRouter = require('./routes/userRoute');
 const productRouter = require('./routes/productRoute');
+const reviewRouter = require('./routes/reviewRoute');
 
 // middleware
 const notFoundMiddleware = require('./middleware/not-found');
@@ -43,6 +44,7 @@ app.get('/api/v1', (req, res) => {
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/products', productRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 // error handling
 app.use(notFoundMiddleware);
