6acb0bc326f358f548162525ad7a797fcd7f0190
Borislav Dostumski <b.dostumski@gmail.com>
2025-02-01 06:43:50 +0200

    return responses

--- programming/javascript/nodejs/e-commerce-api/controllers/reviewController.js
+++ programming/javascript/nodejs/e-commerce-api/controllers/reviewController.js
@@ -3,23 +3,23 @@ const CustomError = require('../errors');
 const StatusCodes = require('http-status-codes');
 
 const createReview = async (req, res) => {
-  req.send('createReview');
+  res.send('createReview');
 }
 
 const getAllReviews = async (req, res) => {
-  req.send('getAllReviews');
+  res.send('getAllReviews');
 }
 
 const getSingleReview = async (req, res) => {
-  req.send('getSingleReview');
+  res.send('getSingleReview');
 }
 
 const updateReview = async (req, res) => {
-  req.send('updateReview');
+  res.send('updateReview');
 }
 
 const deleteReview = async (req, res) => {
-  req.send('deleteReview');
+  res.send('deleteReview');
 }
 
 module.exports = {
