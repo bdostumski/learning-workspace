2c6dddc799686b79525c8ec2e938ca8bac9d3d74
Borislav Dostumski <b.dostumski@gmail.com>
2025-02-04 21:08:23 +0200

    aggregate review model

--- programming/javascript/nodejs/e-commerce-api/models/Review.js
+++ programming/javascript/nodejs/e-commerce-api/models/Review.js
@@ -32,8 +32,31 @@ const ReviewSchema = new mongoose.Schema({
 ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
 
 ReviewSchema.statics.calculateAverageRating = async function(productId) {
-  console.log(productId);
-};
+  const result = await this.aggregate([
+    {
+      $match: { product: productId }
+    },
+    {
+      $group: {
+        _id: null,
+        averageRating: { $avg: '$rating' },
+        numOfReviews: { $sum: 1 },
+      },
+    },
+  ]);
+
+  console.log(result);
+  try {
+    await this.model('Product').findOneAndUpdate({
+      _id: productId
+    }, {
+      averageRating: Math.ceil(result[0]?.averageRating || 0),
+      numOfReviews: result[0]?.numOfReviews || 0,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 ReviewSchema.post('save', async function() {
   await this.constructor.calculateAverageRating(this.product);
