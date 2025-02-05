2aeef01f1ac8f149516d575b7ba56929ce08216c
Borislav Dostumski <b.dostumski@gmail.com>
2025-02-05 05:34:07 +0200

    add order router to app.js

--- programming/javascript/nodejs/e-commerce-api/app.js
+++ programming/javascript/nodejs/e-commerce-api/app.js
@@ -20,6 +20,7 @@ const authRouter = require('./routes/authRoute');
 const userRouter = require('./routes/userRoute');
 const productRouter = require('./routes/productRoute');
 const reviewRouter = require('./routes/reviewRoute');
+const orderRouter = require('./routes/orderRoute');
 
 // middleware
 const notFoundMiddleware = require('./middleware/not-found');
@@ -45,6 +46,7 @@ app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/products', productRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/orders', orderRouter);
 
 // error handling
 app.use(notFoundMiddleware);
