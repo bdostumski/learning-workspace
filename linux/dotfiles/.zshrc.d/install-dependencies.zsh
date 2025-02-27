050d005961b819ba4ee9702ff55e3e216699175c
Borislav Dostumski <b.dostumski@gmail.com>
2025-02-27 22:11:58 +0200

    install dependencies

--- linux/dotfiles/.zshrc.d/install-dependencies.zsh
+++ linux/dotfiles/.zshrc.d/install-dependencies.zsh
@@ -1,46 +1,113 @@
 #!/usr/bin/env zsh
 #
-# STILL WORKING ON THIS SCRIPT 
-#
 # Install common tools for Arch Linux
-
+#
+# Update the system
+sudo pacman -Syu --noconfirm
+#
+# List of packages to install
 packages=(
-	git
-	bat
-	btop
-	htop
-	stow
-	eza
-	ranger
-	neovim
-	emacs
-	vscode
-	kitty
-	maven
-	gradle
-	nodejs
-	docker
-	kubectl
-	minikube
-	kubernetes-cli
-	vagrant
-	virtualbox
-	wine
-	jekyll
-	ruby
-	fzf
-	jq
-	thefuck
+	# Command-Line Utilities
+	git # Version control system
+	zsh # Zsh Shell
+	bat # A cat alternative with syntax highlighting
+	btop # A graphical system monitor (better than htop)
+	htop # An interactive process viewer
+	stow # Manages symlinks (useful for dotfiles)
+	eza # Enhanced ls with git integration
+	ranger # File manager with VI keybindings
+	nnn # A lightweight terminal file manager
+	tmux # Terminal multiplexer
+	ripgrep # Faster alternative to grep for searching files
+	lazygit # Terminal UI for git
+	httpie # A user-friendly HTTP client (better than curl)
+	ncdu # Disk usage analyzer
+	neofetch # Show system information with a logo
+	fastfetch # A faster neofetch alternative. Customization posibbilites are endless.
+	fzf # Fuzzy finder for the command-line
+	jq # JSON processor
+	thefuck # Corrects your previous command
+	wine # Windows compatibility layer
+
+	# Development Tools 
+	neovim # A modern Vim-based text editor
+	emacs # A powerful text editor (Doom Emacs is recommended)
+	kitty # A GPU-accelerated terminal 
+	maven # Java build automation tool
+	gradle # Alternative to Maven for Java builds
+	nodejs # JavaScript runtime
+	npm # Node.js package manager
+	yarn # JavaScript package manager
+	jdk21-openjdk # Java Development Kit 21
+	ruby # Ruby programming language
+	nginx # Web server
+	php # PHP programming language
+	composer # PHP dependency manager
+	python # Python programming language
+	pyenv # Python version manager
+	mysql # Relational database
+	postgresql # Object-relational database
+	redis # In-memory data structure store
+	cmake # Cross-platform build system
+	make # Build automation tool
+	gcc # C and C++ compiler
+	clang # C and C++ compiler
+
+	# Codecs & Fonts
+	ttf-dejavu # DejaVu fonts
+	ttf-liberation # Liberation fonts
+	ttf-roboto # Roboto fonts
+	ttf-ubuntu-font-family # Ubuntu fonts
+	noto-fonts # Noto fonts
+	noto-fonts-emoji # Noto fonts with emoji
+	noto-fonts-cjk # Noto fonts with CJK support
+	ttf-fira-code # Fira Code fonts
+	ttf-fira-mono # Fira Mono fonts
+	ttf-fira-sans # Fira Sans fonts
+	ttf-jetbrains-mono # JetBrains Mono fonts
+	ttf-hack # Hack fonts
+	ttf-inconsolata # Inconsolata fonts
+	nerd-fonts # Nerd Fonts
+	ttf-opensans # Open Sans fonts
+
+	# DevOps & Containeriztion
+	docker # Container engine
+	iptables-nft # Firewall utility
+	minikube # Run Kubernetes locally
+	kubeadm # Kubernetes cluster setup tool
+	kubernetes-control-plane # Kubernetes control plane
+	kubernetes-node # Kubernetes worker nodes
+	kubectl # Kubernetes command-line tool
+	kubelet # Runs on nodes and manages containers
+	containerd # Container runtime
+	helm # Kubernetes package manager
+	vagrant # Virtual machine automation
+	virtualbox # Virtual machine hypervisor
+
+	# GUI Applications
+	firefox # Web browser
+	thunderbird # Email client
+	filezilla # FTP client
+	gimp # Image editor
+	libreoffice # Office suite
+	dbeaver # Database management tool
+	steam # Gaming platform
+	discord # Chat application
+	obs-studio # Screen recording and streaming software
+	kdenlive # Video editor
+
+	# Zsh Plugins
+	zsh-syntax-highlighting
+	zsh-autosuggestions
+	zsh-completions
+	zsh-history-substring-search
 )
 
 # Install packages using pacman
 for package in "${packages[@]}"; do
 	echo "Installing $package..."
-	sudo pacman -S --noconfirm "$package"
+	sudo pacman -S --needed --noconfirm "$package"
 	if [ $? -ne 0 ]; then
 		echo "Error installing $package. Continuing..."
 	fi
 done
-
-
-
