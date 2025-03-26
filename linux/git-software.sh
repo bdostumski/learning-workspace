786fbf3219b8191be135f4bd555f7901be775b8b
Borislav Dostumski <b.dostumski@gmail.com>
2025-03-27 00:04:01 +0200

    add ranger icons

--- linux/git-software.sh
+++ linux/git-software.sh
@@ -5,6 +5,10 @@ fi
 
 bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)" # install zinit
 
+# install icons for ranger file manager
+# echo "default_linemode devicons" >> $HOME/.config/ranger/rc.conf
+git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
+
 # install doom emacs
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs ~/.config/emacs/bin/doom install # install doom emacs
 sudo ln -s /usr/lib/libtree-sitter.so /usr/lib/libtree-sitter.so.0.24 # fix doom emacs error
