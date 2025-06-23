c565c7382a745f274e79f994e6955880a04f4dfb
Borislav Dostumski <b.dostumski@gmail.com>
2025-06-23 20:12:55 +0300

    delete old files

--- linux/dotfiles/.zshrc.d/config.d/env/.env.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-#
-# ENVIRONMENT VARIABLES
-# Description: Expose local environment variables
-# Path: ~/.env.sh
-#
-
-export SECRET_KEY="your_secret_key"
-export API_TOKEN="your_api_token"
